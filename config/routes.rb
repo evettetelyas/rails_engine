9cecc2e556932f89e7438927018998f9d8b87567
Evette Telyas <etelyas@gmail.com>
2019-10-02 21:10:38 -0600

    complete search endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
       get 'merchants/revenue', to: 'merchants/revenues#show'
       get 'merchants/most_revenue', to: 'merchants/most_revenues#index'
+      get 'merchants/find', to: 'merchants/search#show'
+      get 'merchants/find_all', to: 'merchants/search#index'
       resources :merchants, only: [:index, :show] do
         resources :items, module: 'merchants', only: [:index]
         resources :invoices, module: 'merchants', only: [:index]
@@ -13,18 +15,24 @@ Rails.application.routes.draw do
       end
       
       get '/items/most_revenue', to: 'items/most_revenues#index'
+      get 'items/find', to: 'items/search#show'
+      get 'items/find_all', to: 'items/search#index'
       resources :items, only: [:index, :show] do
         resources :invoice_items, module: 'items', only: [:index]
         get '/merchant', to: 'items/merchants#show'
         get '/best_day', to: 'items/best_days#show'
       end
 
+      get '/customers/find', to: 'customers/search#show'
+      get '/customers/find_all', to: 'customers/search#index'
       resources :customers, only: [:index, :show] do
         resources :invoices, module: 'customers', only: [:index]
         resources :transactions, module: 'customers', only: [:index]
         get '/favorite_merchant', to: 'customers/favorite_merchants#show'
       end
 
+      get '/invoices/find', to: 'invoices/search#show'
+      get '/invoices/find_all', to: 'invoices/search#index'
       resources :invoices, only: [:index, :show] do
         resources :transactions, module: 'invoices', only: [:index]
         resources :invoice_items, module: 'invoices', only: [:index]
@@ -33,11 +41,15 @@ Rails.application.routes.draw do
         get '/merchant', to: 'invoices/merchants#show'
       end
 
+      get '/transactions/find', to: 'transactions/search#show'
+      get '/transactions/find_all', to: 'transactions/search#index'
       resources :transactions, only: [:index, :show] do
         get '/invoice', to: 'transactions/invoices#show'
       end
 
-      resources :invoice_items, only: [] do
+      get '/invoice_items/find', to: 'invoice_items/search#show'
+      get '/invoice_items/find_all', to: 'invoice_items/search#index'
+      resources :invoice_items, only: [:index, :show] do
         get '/invoice', to: 'invoice_items/invoices#show'
         get '/item', to: 'invoice_items/items#show'
       end
